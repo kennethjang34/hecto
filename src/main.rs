1a74564f00dff14e7d6602e1c2c43ade94c25ad2
JANG <kennethjang34@gmail.com>
2022-12-25 15:48:43 -0800

    add logger
    using simple_log, emit log infos to console and log file named with current time in %Y-%m-%d_%H-%M-%S format. If error returned during cinitialization of the logger, use env_logger

--- src/main.rs
+++ src/main.rs
@@ -18,12 +18,34 @@ pub use document::Document;
 use editor::Editor;
 pub use editor::Position;
 pub use editor::SearchDirection;
+pub use env_logger;
 pub use filetype::FileType;
 pub use filetype::HighlightingOptions;
+pub use log::{debug, info, trace, warn};
 pub use row::Row;
+pub use simple_log::LogConfigBuilder;
+//use std::time::SystemTime;
+use chrono::Local;
 pub use terminal::Terminal;
 
 fn main() {
+    let config = LogConfigBuilder::builder()
+        .path(format!(
+            "../hecto/log/{}.log",
+            Local::now().format("%Y-%m-%d_%H-%M-%S")
+        ))
+        .size(1 * 100)
+        .roll_count(10)
+        .time_format("%Y-%m-%d %H:%M:%S.%f") //E.g:%H:%M:%S.%f
+        .level("debug")
+        .output_file()
+        .output_console()
+        .build();
+
+    if let Ok(_) = simple_log::new(config) {
+    } else {
+        env_logger::init();
+    }
     let mut editor = Editor::default();
     editor.run();
 }
